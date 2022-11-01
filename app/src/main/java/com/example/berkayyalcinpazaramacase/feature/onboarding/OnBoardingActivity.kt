bad6bc3bd8bdcc876d6b6c52c37a56d843f81bf8
Berkay Yalcin <kemalberkayyalcin@gmail.com>
2022-11-01 19:10:30 +0300

    Onboarding operations complated
    In addition to onboarding operations these added : Onboarding page buttons,  navigation to main from onboarding, showing onboarding to user at the first oppening app

--- app/src/main/java/com/example/berkayyalcinpazaramacase/feature/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/example/berkayyalcinpazaramacase/feature/onboarding/OnBoardingActivity.kt
@@ -1,19 +1,26 @@
 package com.example.berkayyalcinpazaramacase.feature.onboarding
 
+import android.content.Intent
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import androidx.viewpager2.widget.ViewPager2
+import com.example.berkayyalcinpazaramacase.MainActivity
 import com.example.berkayyalcinpazaramacase.R
 import com.example.berkayyalcinpazaramacase.databinding.ActivityOnBoardingBinding
 
 
 import com.example.berkayyalcinpazaramacase.feature.onboarding.adapter.OnBoardingAdapter
+import com.example.berkayyalcinpazaramacase.utils.extensions.gone
+import com.example.berkayyalcinpazaramacase.utils.extensions.visible
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OnBoardingActivity : AppCompatActivity() {
+    private val viewModel by viewModels<OnBoardingViewModel>()
     private lateinit var binding: ActivityOnBoardingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +29,62 @@ class OnBoardingActivity : AppCompatActivity() {
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.viewPager.adapter = OnBoardingAdapter(this,prepareOnBoardingItems())
+        binding.isLastPage = false
+
+        binding.viewPager.adapter = OnBoardingAdapter(this, prepareOnBoardingItems())
 
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
         }.attach()
+
+        initViews()
     }
 
-    private  fun prepareOnBoardingItems(): List<Int>{
-        return listOf(R.layout.item_onboarding,R.layout.item_onboarding_two,R.layout.item_onboarding_three)
+
+
+    private fun initViews() {
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.isLastPage = position == 2
+
+                if (position != 0) {
+                    binding.btnPrev.visible()
+                } else {
+                    binding.btnPrev.gone()
+                }
+            }
+        })
+
+        binding.btnSkip.setOnClickListener {
+            if (binding.viewPager.currentItem == 2) {
+                skipOnBoarding()
+            } else {
+
+                binding.viewPager.setCurrentItem(binding.viewPager.currentItem.plus(1), true)
+            }
+        }
+        binding.btnPrev.setOnClickListener {
+            binding.viewPager.setCurrentItem(binding.viewPager.currentItem.minus(1), true)
+        }
+
+    }
+    private fun skipOnBoarding() {
+        viewModel.setOnBoardingStatus()
+        navigateToMain()
+    }
+
+    private fun prepareOnBoardingItems(): List<Int> {
+        return listOf(
+            R.layout.item_onboarding,
+            R.layout.item_onboarding_two,
+            R.layout.item_onboarding_three
+        )
+    }
+
+    private fun navigateToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/berkayyalcinpazaramacase/feature/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/berkayyalcinpazaramacase/feature/splash/SplashViewModel.kt
@@ -1,5 +1,43 @@
 package com.example.berkayyalcinpazaramacase.feature.splash
 
-class SplashViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.berkayyalcinpazaramacase.data.local.DataStoreManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@HiltViewModel
+class SplashViewModel @Inject constructor(private val dataStoreManager: DataStoreManager) :
+    ViewModel() {
+
+    private val _uiEvent = MutableSharedFlow<SplashViewEvent>(replay = 0)
+    val uiEvent: SharedFlow<SplashViewEvent> = _uiEvent
+
+
+    init {
+        checkOnBoardingVisibleStatus()
+    }
+
+    private fun checkOnBoardingVisibleStatus() {
+        viewModelScope.launch {
+            dataStoreManager.getOnBoardingVisible.collect {
+                if (it) {
+                    _uiEvent.emit(SplashViewEvent.NavigateToMain)
+                    // Navigate to main activity
+                } else {
+                    _uiEvent.emit(SplashViewEvent.NavigateToOnBoarding)
+                    // Navigate to on boarding activity
+                }
+            }
+        }
+    }
+}
+
+sealed class SplashViewEvent {
+    object NavigateToLogin : SplashViewEvent()
+    object NavigateToOnBoarding : SplashViewEvent()
+    object NavigateToMain : SplashViewEvent()
 }
